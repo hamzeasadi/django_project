06ae01d468d42908333c90ccb27d41c06ccede53
Hamzeh Asadi <hamzeasadi1@gmail.com>
2021-02-09 02:33:21 +0000

    create a post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    """simple post class"""
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    # if the user deleted this post will be deleted as well
+
+
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views as blog_views
 
 urlpatterns = [
-    path('', blog_views.home, name='blog-home')
+    path('', blog_views.home, name='blog-home'),
+    path('about/', blog_views.about, name='blog-about'),
 ]
