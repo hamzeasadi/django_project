34bb440a3d99ad46169a74624159ad51a5869744
Hamzeh Asadi <hamzeasadi1@gmail.com>
2021-02-09 09:48:04 +0000

    add form to registration

--- users/views.py
+++ users/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.shortcuts import redirect
 
 def registration(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            myMessage = f'account created for {username}'
+            messages.success(request, myMessage)
+            return redirect(to='blog-home')
+
+    else:
+        form = UserCreationForm()
+
     return render(request, 'users/registration.html', {'form': form})
