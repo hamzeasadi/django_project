20c423e607214404d8e9966e31451b3cfdce22ba
Hamzeh Asadi <hamzeasadi1@gmail.com>
2021-02-09 12:36:23 +0000

    add signal function

--- users/admin.py
+++ users/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models as m
 
 admin.site.register(m.Profile)
+
+
--- users/signals.py
+++ users/signals.py
@@ -1,1 +1,23 @@
+# we import post_save so it will fired when object(post) is saved
 from django.db.models.signals import post_save
+
+# it is the sender because it sends the signal
+from django.contrib.auth.models import User
+
+# we will create a receiver, a function, that get a signal and perform some task
+from django.dispatch import receiver
+
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+
